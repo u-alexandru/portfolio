d83129ac61854e31a9bee1caf4bcb44ae127b1e3
Alexandru Ungureanu <u95.alexandru@gmail.com>
2024-11-06 16:39:15 +0200

    feat: test

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module Chess
+
+go 1.23.2
+
+require github.com/mattn/go-sqlite3 v1.14.24 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,68 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 )
 
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+type Room struct {
+	Id         int       `json:"id"`
+	RoomNumber string    `json:"room_number"`
+	InProgress int       `json:"in_progress"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+func InitDB() *sql.DB {
+	once.Do(func() {
+		var err error
+		db, err = sql.Open("sqlite3", "./mydatabase.sqlite")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create tables if they don't exist
+		_, err = db.Exec(`
+			CREATE TABLE IF NOT EXISTS rooms (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				room_number TEXT NOT NULL UNIQUE,
+				in_progress INTEGER NOT NULL DEFAULT 1,
+				created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+			)
+		`)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	return db
+}
+
+func getRoomHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		err := r.ParseForm()
+		if err != nil {
+			return
+		}
+		fmt.Println("POST ROOM")
+		fmt.Println(r.Form.Get("room_number"))
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<p>Success</p>"))
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	tmpl, err := template.ParseFiles("frontend/index.html")
 
 	if err != nil {
@@ -19,7 +76,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		Name: "Go Developer",
 	}
 
-	tmpl.Execute(w, data)
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		return
+	}
 }
 
 func staticFileServer(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +87,17 @@ func staticFileServer(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/static/", staticFileServer)
+	InitDB()
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
 
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/static/", staticFileServer)
+	http.HandleFunc("/rooms/get", getRoomHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 
